c9579ff4a98fc932360b47b4d65cebef40373bd9
ManuelAlejandro <manuelalejandrogamboa35@gmail.com>
2023-09-29 11:57:31 +0200

    initialize

--- Algoritms_/Algoritms_/PLJ.cs
+++ Algoritms_/Algoritms_/PLJ.cs
@@ -2,6 +2,30 @@
 {
     public class PLJ
     {
+        public bool Satisfy(int cardinality, List<(List<int>, List<int>)> dependencies)
+        {
+            bool[,] arr = new bool[dependencies.Count, cardinality];
 
+            InitializeArr(arr, dependencies);
+
+            return false;
+        }
+
+
+        private void InitializeArr(bool[,] arr, List<(List<int>, List<int>)> dependencies)
+        {
+            foreach (var dependence in dependencies)
+            {
+                foreach (var item in dependence.Item1)
+                {
+                    arr[dependencies.IndexOf(dependence), item] = true;
+                }
+
+                foreach (var item in dependence.Item2)
+                {
+                    arr[dependencies.IndexOf(dependence), item] = true;
+                }
+            }
+        }
     }
 }
